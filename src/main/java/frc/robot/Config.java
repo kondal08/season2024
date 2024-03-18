f9f1d97a9b6598a9352273e6945c8faad08d8253
kondal08 <konstantinosd08@gmail.com>
2024-03-18 09:31:32 +0000

    Updated to match correct version

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -9,28 +9,30 @@ public class Config {
   public static final boolean IS_ALLIANCE_RED = true;
   public static final boolean IS_ALLIANCE_BLUE = !IS_ALLIANCE_RED;
   public static final class Subsystems {
-    public static final boolean DRIVETRAIN_ENABLED = true;
-    public static final boolean CLIMBER_ENABLED = false;
-    public static final boolean SHOOTER_ENABLED = true;
-    public static boolean SHOOT_MOVING = false;
-
-    public static final class Intake {
+      public static final boolean DRIVETRAIN_ENABLED = true;
+      public static final boolean CLIMBER_ENABLED = false;
+      public static final boolean SHOOTER_ENABLED = true;
+      public static boolean SHOOT_MOVING = false;
+      public static boolean VISION_ENABLED = true;
+      public static final boolean PIVOT_ENABLED = true;
       public static final boolean INTAKE_ENABLED = true;
-      public static final boolean INTAKE_SENSOR_ENABLED = true;
-    }
+      public static final boolean FEEDER_ENABLED = true;
 
-    public class Controllers {
-        public static final boolean DRIVER_ENALBED = true;
-        public static final boolean JOYSTICK_OPERATOR_ENABLED = false;
-        public static final boolean OPERATOR_ENABLED = true;
-        public static final boolean BOARD_OPERATOR_ENABLED = true;
+      public static final boolean LEDs_ENABLED = false;
 
-        public static ButtonMap getDriverController() {
-            return new Xbox();
-        }
+      public class Controllers {
+          public static final boolean DRIVER_ENABLED = true;
+          public static final boolean JOYSTICK_OPERATOR_ENABLED = false;
+          public static final boolean OPERATOR_ENABLED = true;
+          public static final boolean BOARD_OPERATOR_ENABLED = true;
 
-        public static ButtonMap getOperatorController() {
-            return BOARD_OPERATOR_ENABLED ? new BoardController() : new Xbox();
-        }
-    }
+          public static ButtonMap getDriverController() {
+              return new Xbox();
+          }
+
+          public static ButtonMap getOperatorController() {
+              return BOARD_OPERATOR_ENABLED ? new BoardController() : new Xbox();
+          }
+      }
+  }
 }
